ccf4935f7d439a84e602a69286cf98f945444026
Rudy <s60990340@gmail.com>
2026-01-10 01:59:14 +0800

    更正icon 品牌

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 // 2. 引入 Bootstrap 的 JS (互動功能，如下拉選單)
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// 人工補充 feather icon
+import feather from "feather-icons";
 
 // 3. 引入你自己的 CSS (客製化樣式)
+
 import "./style.css";
+
+feather.replace();
