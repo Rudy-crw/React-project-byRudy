31b07aee3efec5d5a8ef7adc78b13fdc8fe8975a
Rudy <s60990340@gmail.com>
2026-01-10 01:14:42 +0800

    新增 gh設定,修改 section-title 背景英文字置中

---
+++ src/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // 👇 這裡填入 '/你的 GitHub 儲存庫名稱/' (前後都要有斜線)
+  // 例如你的 repo 叫 pet-rare-guide，就填 '/pet-rare-guide/'
+  base: "/React-project-byRudy/",
+});
