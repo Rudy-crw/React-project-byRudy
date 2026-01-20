b58dfe7c879d361dba187116f31d494669caa20e
RudyWu <Rudy.Wu@outlook.com>
2026-01-20 17:17:26 +0800

    vite-config 設定 store-detail 可以產生gh-pages頁面

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ export default defineConfig({
   // 👇 這裡填入 '/你的 GitHub 儲存庫名稱/' (前後都要有斜線)
   // 例如你的 repo 叫 pet-rare-guide，就填 '/pet-rare-guide/'
   base: "/React-project-byRudy/",
+  build: {
+    rollupOptions: {
+      input: {
+        // 2. 設定你的入口檔案
+        main: resolve(__dirname, "index.html"), // 首頁
+        store: resolve(__dirname, "store-detail.html"), // 你的第二頁 (名稱 store 可以自己取)
+      },
+    },
+  },
 });
